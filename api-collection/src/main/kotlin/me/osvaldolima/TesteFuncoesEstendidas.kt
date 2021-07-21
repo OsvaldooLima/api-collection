f2e1c078c0e2ef5bcccf25cf0a757b212efb9481
OsvaldooLima <oc_juniorlima@hotmail.com>
2021-07-21 10:46:13 -0300

    Primeiro commit

---
+++ api-collection/src/main/kotlin/me/osvaldolima/TesteFuncoesEstendidas.kt
@@ -0,0 +1,13 @@
+package me.osvaldolima
+
+fun  main () {
+    val salarios = arrayOf(" 2000 ".toBigDecimal(), " 1500 ".toBigDecimal()," 4000 ".toBigDecimal())
+
+    println ( " ---------------------- " )
+    println (salarios.somatoria ())
+
+    println ( " ---------------------- " )
+    println (salarios.media ())
+}
+
+
